7fb3b0ecf9aace43b0e0fcd4133dc6f9ffc0a167
Julien Jamet <julien.jamet.34@gmail.com>
2022-11-04 14:00:44 +0100

    addition of the javascript files

---
+++ frontend/js/script.js
@@ -0,0 +1,38 @@
+/****************
+****VARIABLES****
+****************/
+
+
+/*La variable qui va recevoir le contenu HTML*/
+const items = document.querySelector("section.items");
+
+
+/****************
+****FONCTIONS****
+****************/
+
+
+/*La fonction qui va récupérer les données de l'API
+puis les injecter dans le HTML*/
+function getApi_index() {
+    fetch("http://localhost:3000/api/products")
+/*Test de validation : si OK, récupération des données de l'API*/
+    .then(function(res) {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+    .then(function(value) {
+/*Pour chaque itération du tableau de valeurs qu'on vient de récupérer...*/
+      for (let each of value) {
+/*On injecte du code HTML dynamique dans notre variable*/
+        items.innerHTML += (`<a href="./frontend/html/product.html?id=${each._id}#item"><article><img src="${each.imageUrl}"/><h3 class='productName'>${each.name}</h3><p class='productDescription'>${each.description}</p></article></a>`);
+      }
+    })
+    .catch(function(err) {
+    });
+}
+/*La fonction se déclenche au chargement de la page*/
+window.addEventListener("DOMContentLoaded", function() {
+    getApi_index();
+})
